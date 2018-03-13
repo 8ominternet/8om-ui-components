3ac5681b8f2e5dd6ad27c6169ea0af9ce87b2e03
Harsh Maur <harshmaur@gmail.com>
2018-03-13 15:32:08 +0530

    added knobs

--- stories/button.js
+++ stories/button.js
@@ -3,17 +3,21 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { WithNotes } from '@storybook/addon-notes'
+import { select, withKnobs } from '@storybook/addon-knobs/react'
 
 import Button from '../src/button'
 
-storiesOf('Button', module)
+const stories = storiesOf('Button', module)
+stories.addDecorator(withKnobs)
+
+stories
   .add('normal state', () => (
     <WithNotes notes="This is the normal state of the buttons">
-      <Button onClick={action('clicked')} />
+      <Button onClick={action('clicked')} type="primary" />
     </WithNotes>
   ))
   .add('with message', () => (
     <WithNotes notes={`Using prop "message" to change the button text`}>
-      <Button onClick={action('clicked')} message="Something" />
+      <Button onClick={action('clicked')} type={select('message', { red: 'Red', green: 'Green' }, 'red')} />
     </WithNotes>
   ))
