0d1f6fcdf8c55017602f2d53d71185ff16453aa4
kumarchandan41 <kumar.chandan41m@gmail.com>
2018-03-14 13:20:54 +0530

    change struct

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React, { Component } from 'react'
-
-export default class extends Component {
-  render() {
-    return (
-      <div>
-        <h2>{this.props.message}</h2>
-      </div>
-    )
-  }
-}
+export * as Button from './Button'
