46003e4b90eb3a574608caa121a4bda923e02aca
Harsh Maur <harshmaur@gmail.com>
2018-03-28 17:01:02 +0530

    change config for storybook build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,7 +2,13 @@ var config = require('create-nwb-webpack-config')()
 
 // We need to filter out any chunking plugins, and also the HtmlWebpackPlugin as it conflicts.
 config.plugins = config.plugins.filter(function(plugin) {
+  // remove uglify js plugin
+  if (plugin.options && plugin.options.sourceMap) {
+    return false
+  }
   return !plugin.chunkNames && !(plugin.options && plugin.options.template)
 })
+
+console.log(config.plugins)
 
 module.exports = config
